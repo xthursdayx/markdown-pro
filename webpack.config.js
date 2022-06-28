4130eef84c2830df3155a02fe820c9c55a542b04
xthursdayx <xthursdayx@mailbox.org>
2022-06-28 14:54:31 -0400

    Update to head

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: [
-    path.resolve(__dirname, "src", "main.js"),
-    path.resolve(__dirname, "src", "main.scss")
-  ],
+  entry: [path.resolve(__dirname, 'src', 'main.js'), path.resolve(__dirname, 'src', 'main.scss')],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "dist.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'dist.js',
   },
   module: {
     rules: [
@@ -18,14 +15,12 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: [
-                  "src/main.scss"
-                ],
+                includePaths: ['src/main.scss'],
               },
             },
           },
@@ -38,29 +33,41 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      }
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "node_modules/easymde/dist/easymde.min.js", to: path.resolve(__dirname, "dist/vendor/easymd/easymd.js") },
-        { from: "node_modules/easymde/dist/easymde.min.css", to: path.resolve(__dirname, "dist/vendor/easymd/easymde.css") },
-        { from: "node_modules/highlightjs/highlight.pack.min.js", to: path.resolve(__dirname, "dist/vendor/highlightjs/highlightjs.js") },
-        { from: "node_modules/@standardnotes/component-relay/dist/dist.js", to: path.resolve(__dirname, "dist/lib/component-relay.js") },
-        { from: "node_modules/sn-stylekit/dist/stylekit.css", to: path.resolve(__dirname, "dist/stylekit.css") },
+        {
+          from: require.resolve('easymde/dist/easymde.min.js'),
+          to: path.resolve(__dirname, 'dist/vendor/easymd/easymd.js'),
+        },
+        {
+          from: require.resolve('easymde/dist/easymde.min.css'),
+          to: path.resolve(__dirname, 'dist/vendor/easymd/easymde.css'),
+        },
+        {
+          from: require.resolve('highlightjs/highlight.pack.min.js'),
+          to: path.resolve(__dirname, 'dist/vendor/highlightjs/highlightjs.js'),
+        },
+        {
+          from: require.resolve('@standardnotes/component-relay/dist/dist.js'),
+          to: path.resolve(__dirname, 'dist/lib/component-relay.js'),
+        },
+        { from: require.resolve('sn-stylekit/dist/stylekit.css'), to: path.resolve(__dirname, 'dist/stylekit.css') },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: "dist.css"
+      filename: 'dist.css',
     }),
     new HtmlWebpackPlugin({
-      title: "Markdown Pro",
-      template: "editor.index.ejs"
-    })
+      title: 'Markdown Pro',
+      template: 'editor.index.ejs',
+    }),
   ],
-};
+}
